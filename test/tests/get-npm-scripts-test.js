86e341e75d155ea401f3fe94bc2ecafa3339c8ae
Kevin Phillips <kphillips86@gmail.com>
2017-02-06 16:29:45 -0600

    showing error message if package.json is malformed

--- test/tests/get-npm-scripts-test.js
+++ test/tests/get-npm-scripts-test.js
@@ -16,4 +16,14 @@ describe('getNpmScripts', function() {
   it('returns empty object if package.json does not exist', function() {
     assert.deepEqual(getNpmScripts(__dirname), {}, 'should be an empty object');
   });
+
+  it('throws an error if package.json is malformed', function() {
+    var root = path.join(__dirname, '..', 'bad-package-json');
+
+    try {
+      Object.keys(getNpmScripts(root));
+    } catch(e) {
+      assert.ok(/SyntaxError/.test(e), 'SyntaxError thrown');
+    }
+  });
 });
